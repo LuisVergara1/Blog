0fe4a81671643004bfc82049ff35ba95da756377
LuisVergara1 <luismarcelo.v.c@hotmail.com>
2023-10-10 15:29:10 -0300

    implementando Swagger-ui

--- src/main/java/com/Blog/BlogApplication.java
+++ src/main/java/com/Blog/BlogApplication.java
@@ -2,7 +2,12 @@ package com.Blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -11,5 +16,18 @@ public class BlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
+
+	@Bean
+	 public OpenAPI customOpenApi(){
+		return new OpenAPI()
+		.info(new Info()
+		.title("Sistema de Blog")
+		.version("0.1.1")
+		.description("EndPoint del Blog")
+		.termsOfService("http://swagger.io/terms/")
+		.license(new License().name("Apache 2.0").url("http:springdoc.org")));
+	 }
+
+
 
 }
