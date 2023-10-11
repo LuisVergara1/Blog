1c927f4363914433fa6a3739f4f7d4432fb13503
LuisVergara1 <luismarcelo.v.c@hotmail.com>
2023-10-11 19:59:33 -0300

    Modificacion UserControllers y Documentacion

--- src/main/java/com/Blog/BlogApplication.java
+++ src/main/java/com/Blog/BlogApplication.java
@@ -23,7 +23,7 @@ public class BlogApplication {
 		.info(new Info()
 		.title("Sistema de Blog")
 		.version("0.1.1")
-		.description("EndPoint del Blog")
+		.description("EndPoint del Blog <br> Los EndPoint Se Modificaran una ver se Agrege SpringSecurity")
 		.termsOfService("http://swagger.io/terms/")
 		.license(new License().name("Apache 2.0").url("http:springdoc.org")));
 	 }
--- src/main/java/com/Blog/controller/UsuarioController.java
+++ src/main/java/com/Blog/controller/UsuarioController.java
@@ -1,9 +1,8 @@
 package com.Blog.controller;
-
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,11 +10,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.Blog.DTO.UserCreated;
 import com.Blog.entity.Usuario;
 import com.Blog.service.usuario.UsuarioService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -30,7 +27,7 @@ public class UsuarioController {
     @Operation(summary = "Crear Usuario",
                description = "Crea un nuevo usuario con el rol por defecto 'Usuario'.")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Usuario creado exitosamente"),
+        @ApiResponse(responseCode = "200", description = "Usuario creado exitosamente" ),
         @ApiResponse(responseCode = "400", description = "Solicitud inválida")
     })
     @PostMapping("/created")
@@ -44,47 +41,67 @@ public class UsuarioController {
         }
     }
 
+    @Operation(summary = "Listar Usuarios",
+               description = "Permite Listar Todos Los Usuarios solo a los Rol 'Administrador'.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Usuario creado exitosamente"),
+        @ApiResponse(responseCode = "400", description = "Solicitud inválida")
+    })
+    @GetMapping("{idUser}/all")
+    public ResponseEntity<?>getall(@PathVariable("idUser")Long id){
 
-    @GetMapping("/all")
-    public ResponseEntity<List<Usuario>>getall(){
-        List<Usuario> usuarios = usuarioService.listarUsuario();
+        Usuario usuarioEncontrado = usuarioService.obteneUsuario(id);
+        if(usuarioEncontrado.getRol().equals("Admin"))
+        {List<Usuario> usuarios = usuarioService.listarUsuario();
 
         return new ResponseEntity<>(usuarios,HttpStatus.OK);
     }
-
-    @Operation(summary = "Buscar Usuario",
-               description = "Busca Un Usuario Segun su ID.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Usuario Encontrado"),
-        @ApiResponse(responseCode = "404", description = "Usuario no Encontrado")
-    })
-    @GetMapping("/{id}")
-    public ResponseEntity<?>getUserById(@PathVariable("id") Long id)
-    {
-        Usuario usuarioEncontrado = usuarioService.obteneUsuario(id);
-        if(usuarioEncontrado !=null ){
-        return new ResponseEntity<>(usuarioEncontrado,HttpStatus.OK);
-        }
-        return new ResponseEntity<>("Usuario No Encontrado",HttpStatus.NOT_FOUND);
-    
+        return new ResponseEntity<>("Acceso Denegado",HttpStatus.UNAUTHORIZED);        
     }
 
 
+
+    @Operation(summary = "Recuperar Usuario por ID",
+    description = "Recupera un usuario por su ID. Los usuarios pueden recuperar su propia información o, si son administradores, acceder a la información de otros usuarios."
+    + "<br> Donde idUser : es el Usuario Activo <br> idFound: Usuario a Buscar")  
+     @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Usuario Encontrado" , headers = { }),
+        @ApiResponse(responseCode = "401", description = "Sin Autorizacion"),
+        @ApiResponse(responseCode = "404", description = "Usuario no Encontrado")
+    })
+    @GetMapping("/{idUser}/{idFound}")
+    public ResponseEntity<?>getUserById(@PathVariable("idUser") Long id,@PathVariable("idFound")Long idFound)
+    {
+        Usuario usuarioEncontrado = usuarioService.obteneUsuario(id);
+        Usuario usuarioFound = usuarioService.obteneUsuario(idFound);
+
+        if (usuarioFound == null ||  usuarioEncontrado == null) {
+            return new ResponseEntity<>("Usuario No Encontrado", HttpStatus.NOT_FOUND);
+        }   
+
+        if (usuarioEncontrado.getRol().equals("Admin") || usuarioEncontrado.getId_usuario() == idFound) {
+            return new ResponseEntity<>(usuarioFound, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("No Tienes Permisos Para Buscar Este Usuario", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
     @Operation(summary = "Modificar Usuarios",
-               description = "Modifica Usuario el Id es del Usuario Actual de la Session")
+               description = "Se puede Modificar usuarios de dos formas:<br> 1) Un administrador puede modificar cualquier usuario usando su propio id como referencia, incluyendo la capacidad de cambiar su rol.<br>"+ 
+                              "2) Un usuario puede modificar sus propios datos, pero no puede cambiar su propio rol.<br>"+" Datos no Modificables : <br> username <br> correo ")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Usuario Modificado"),
         @ApiResponse(responseCode = "304", description = "Usuario no Modificado"),
         @ApiResponse(responseCode = "404", description = "Usuario no Encontrado"),
         @ApiResponse(responseCode = "401", description = "Sin Permisos Necesarios")
     })
-    @PutMapping("/{id}/modificar")
-    public ResponseEntity<String>modificarUsuario(@PathVariable("id")Long id , @RequestBody Usuario usuarioModificado)
+    @PutMapping("/{idUser}/modificar")
+    public ResponseEntity<String>modificarUsuario(@PathVariable("idUser")Long id , @RequestBody Usuario usuarioModificado)
     {
        Usuario usuarioencontrado = usuarioService.obteneUsuario(id);
 
        if(usuarioencontrado!=null){
-       if(usuarioencontrado.getRol().equals("Admin")|| usuarioencontrado.getId_usuario()==usuarioModificado.getId_usuario()){
+       if(usuarioencontrado.getRol().equals("Admin")|| usuarioencontrado.getId_usuario() == usuarioModificado.getId_usuario()){
          Usuario usuarioActualizado = usuarioService.modificarUsuario(id, usuarioModificado);
          if(usuarioActualizado!=null){
             return new ResponseEntity<>("Usuario Actualizado",HttpStatus.OK);
@@ -99,6 +116,31 @@ public class UsuarioController {
         return new ResponseEntity<>("Id De Usuario no Valido",HttpStatus.NOT_FOUND);
        }
        return new ResponseEntity<>("No Tienes Permisos para Modificar este Usuario",HttpStatus.UNAUTHORIZED);
-  
     }
+
+
+    @DeleteMapping("/{idUser}/{idDelete}")
+    public ResponseEntity<?>eliminarUsuario(@PathVariable("idUser")Long id , @PathVariable("idDelete")Long idDelete)
+    {
+        Usuario usuarioEncontrado = usuarioService.obteneUsuario(id);
+        Usuario usuarioDelete = usuarioService.obteneUsuario(idDelete);
+         if (usuarioEncontrado == null ||  usuarioDelete == null) {
+            return new ResponseEntity<>("Usuario No Encontrado", HttpStatus.NOT_FOUND);
+        }   
+
+        if (usuarioEncontrado.getRol().equals("Admin") || usuarioEncontrado.getId_usuario() == idDelete) {
+            Boolean result = usuarioService.eliminarUsuario(idDelete);
+            if(result ==true){
+            return new ResponseEntity<>("Usuario Eliminado", HttpStatus.OK);
+            }else{
+                return new ResponseEntity<>("No se a podido Eliminar el Usuario", HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<>("No Tienes Permisos Para Buscar Este Usuario", HttpStatus.UNAUTHORIZED);
+        }
+
+
+    }
+
+
 }
